c282640c55378f1a4d2801bf1e90fc31c590aeda
Aiden Wake <15WakeA@fulford.york.sch.uk>
2021-12-14 10:16:59 +0000

    Add project files.

---
+++ TableLayout/Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace TableLayout
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.table = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // table
+            // 
+            this.table.ColumnCount = 2;
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.table.Location = new System.Drawing.Point(0, 0);
+            this.table.Name = "table";
+            this.table.RowCount = 2;
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.table.Size = new System.Drawing.Size(800, 450);
+            this.table.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.table);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel table;
+    }
+}
+
---
+++ TableLayout/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TableLayout
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table.RowCount = 10;
+            table.ColumnCount = 10;
+
+
+            table.SuspendLayout();
+            for(int y = 0; y < 10; y++)
+            {
+                table.RowStyles[y].Height = 10;
+                table.RowStyles[y].SizeType = SizeType.Percent;
+                for (int x = 0; x < 10; x++)
+                {
+                    table.ColumnStyles[x].Width = 10;
+                    table.ColumnStyles[x].SizeType = SizeType.Percent;
+                    Button b = new Button();
+                    b.Text = $"X: {x} Y: {y}";
+                    table.Controls.Add(b);
+                    
+                }
+            }
+            table.Dock = DockStyle.Fill;
+            table.ResumeLayout();
+            
+            
+            
+        }
+    }
+}
